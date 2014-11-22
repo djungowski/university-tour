213a43b3ed5214b1df4ded7a5f3c618e5f0dd986
stehrenberg <sehrenbe@hm.edu>
2014-11-22 16:17:05 +0100

    Stand Tutorial Steff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	void FixedUpdate() {
+		float moveHorizontally = Input.GetAxis("Horizontal");
+		float moveUp = 0;
+		float moveVertically = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontally, moveUp, moveVertically);
+		rigidbody.AddForce(movement);
+		Debug.Log("moving: " + movement);
+	}
+}
