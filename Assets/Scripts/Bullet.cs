9c144e61a1f04520b22c753dd841be21966e08d4
stehrenberg <sehrenbe@hm.edu>
2014-11-22 12:03:59 +0100

    Domis Rumspielerei

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float Speed = 2000.0f;
+
+	public GameObject explosionPrefab;
+
+	void onCollisionEnter(Collision c) {
+		Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			GameObject g = (GameObject)Instantiate(bulletPrefab, transform.position, transform.parent.rotation);
+			float force = g.GetComponent<Bullet>().Speed;
+			g.rigidbody.AddForce(g.transform.forward * force);
+		}
+	}
+}
