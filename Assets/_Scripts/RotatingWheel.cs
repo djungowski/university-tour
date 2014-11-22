1bb8320ec0b6537796281b470791eb0d5fc11508
Dominik Jungowski <Dominik@Jungowski.de>
2014-11-22 21:57:40 +0100

    Jede Menge Assets

---
+++ Assets/_Scripts/RotatingWheel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingWheel : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		float speed = Input.GetAxis ("Vertical");
+		Vector3 rotation = new Vector3 (1.0f, 0.0f, 0.0f);
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			transform.Rotate(rotation, Space.Self);
+		}
+	}
+}
