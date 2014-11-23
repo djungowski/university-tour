0c40c0398eea23af695a7847092692c2fbfd9290
Dominik Jungowski <dominik@jungowski.de>
2014-11-23 21:23:02 +0100

    Bomben werfen, fuck yeah :D

---
+++ Assets/_Scripts/Shoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bombPrefab;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightControl)) {
+			Vector3 currentPosition = transform.position;
+			Vector3 targetSpawnPosition = currentPosition + new Vector3(0.1f, 1.0f, 7.0f);
+			Quaternion rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+
+			GameObject bomb = (GameObject) Instantiate(bombPrefab, targetSpawnPosition, rotation);
+
+			Vector3 direction = new Vector3(0.0f, 1.0f, 1.0f);
+			float force = 300.0f;
+			bomb.rigidbody.AddForce(direction * force);
+		}
+	}
+}
